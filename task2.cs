4479783192aa1d8e528427119eb1f150953e2672
Михаил <74896319+Smoo0ke@users.noreply.github.com>
2022-08-11 21:36:26 +1000

    Create task2.cs

---
+++ task2.cs
@@ -0,0 +1,11 @@
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+    Console.WriteLine("Число " + num + " четное");
+}
+else
+{
+    Console.WriteLine("Число " + num + " нечетное");
+}
